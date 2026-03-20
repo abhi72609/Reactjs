434915e6149ae7cda44f9f75877fb131c0d192da
Abhishek Raj <abhishekraj93088@gmail.com>
2026-03-20 20:43:56 +0530

    update

--- P7/src/Components/Hooks/State.jsx
+++ P7/src/Components/Hooks/State.jsx
@@ -1,6 +1,26 @@
 // Lecture :- #26 [React States: What is useState Hook & Why we need it in React] 
-import { useState } from "react";
+// import { useState } from "react";
 
+// export const State = () => {
+//     const [count, setCount] = useState(0);
+//     const handleButtonClick = () => {
+//         setCount(() => count + 1);
+//     };
+    
+//     return(
+//     <>
+//         <section className="main-div">
+//             <h1>{count}</h1>
+//             <button onClick={handleButtonClick}>Increment</button>
+//         </section>
+//     </>
+//     );
+// };
+
+
+//  Lecture :- #27 [    How React State Works: In-depth Guide with Theory, Animation, and Practical Examples    ]
+
+import { Children, useState } from "react";
 export const State = () => {
     const [count, setCount] = useState(0);
     const handleButtonClick = () => {
@@ -9,10 +29,11 @@ export const State = () => {
     
     return(
     <>
-        <section className="main-div">
+        <div className="main-div">
             <h1>{count}</h1>
             <button onClick={handleButtonClick}>Increment</button>
-        </section>
+        </div>
+        <ChildComp />
     </>
     );
 };
