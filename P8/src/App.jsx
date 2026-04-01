9b7fb72d0282272a5025b35d0928cbfc1ed75046
Abhishek Raj <abhishekraj93088@gmail.com>
2026-04-01 17:48:03 +0530

    update

--- P8/src/App.jsx
+++ P8/src/App.jsx
@@ -1,11 +1,13 @@
-import { DerivedState } from "./Components/Hooks/DerivedState";
+// import { DerivedState } from "./Components/Hooks/DerivedState";
+import { LiftStateUp } from "./Components/LiftStateUp";
 import "./Components/EV.css";
 
 export const App = () => {
     
     return(
         <section className="container">
-            <DerivedState />
+            {/* <DerivedState /> */}
+            <LiftStateUp />
         </section>
     );
 };
