2a3bbd214fa6e92b76084fadd14a5ca39591a833
Abhishek Raj <abhishekraj93088@gmail.com>
2026-04-03 00:46:19 +0530

    update

--- P8/src/App.jsx
+++ P8/src/App.jsx
@@ -1,13 +1,16 @@
 // import { DerivedState } from "./Components/Hooks/DerivedState";
-import { LiftStateUp } from "./Components/LiftStateUp";
-import "./Components/EV.css";
+// import { LiftStateUp } from "./Components/LiftStateUp";
 
+import { ToggleSwitch } from "./Project1/ToggleSwitch/ToggleSwitch";
+import "./Components/EV.css";
 export const App = () => {
     
     return(
         <section className="container">
-            {/* <DerivedState /> */}
+            {/* <DerivedState />
             <LiftStateUp />
+            */}
+            <ToggleSwitch />
         </section>
     );
 };
